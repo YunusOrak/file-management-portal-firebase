94354be6a2aa84d4d23d891f702b8e870746c643
YunusOrak <40091165+YunusOrak@users.noreply.github.com>
2023-01-28 09:13:44 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase: {
-    projectId: 'file-management-portal',
-    appId: '1:79829743811:web:e991c938515a2ea3c78a76',
-    storageBucket: 'file-management-portal.appspot.com',
-    apiKey: 'AIzaSyB1ltiytyFNCc6mvARNlZjK8oxpkFSFefU',
-    authDomain: 'file-management-portal.firebaseapp.com',
-    messagingSenderId: '79829743811',
-    measurementId: 'G-CHTH68MWN7',
-  },
-  production: false
+
 };
 
 /*
